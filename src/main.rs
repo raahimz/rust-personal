2d58a759825701e14f21d5ed0c4f51be60da6a19
Raahim Zeeshan <raahim.home@gmail.com>
2022-09-02 19:54:41 +0500

    add linux speed

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut ssid = String::from("");
     let mut ip = String::from("");
     let mut bssid = String::from("");
-    
+
     let output = Command::new("ipconfig")
         .arg("getsummary")
         .arg("en0")
@@ -13,9 +13,9 @@ fn main() {
         .expect("failed to execute process");
 
     let output = String::from_utf8(output.stdout).expect("invalid utf8");
-    let output: Vec<&str> = output.split("\n").collect();
+    let output: Vec<&str> = output.trim().split(" ").collect();
 
-    for word in output {
+    for word in &output {
         let key_value: Vec<&str> = word.trim().split(":").collect();
         
         // Getting SSID
@@ -48,6 +48,7 @@ fn main() {
     let mut bssid = String::from("");
     let mut frequency = String::from("");
     let mut signal_level = String::from("");
+    let mut speed = String::from("");
 
     // Getting IP
     let output = Command::new("hostname")
@@ -57,6 +58,7 @@ fn main() {
         
     ip = String::from_utf8(output.stdout).expect("invalid utf8");
 
+    // Getting rest of the data
     let output = Command::new("iwconfig")
         .output()
         .expect("failed to execute process");
@@ -93,78 +95,16 @@ fn main() {
             let mut value = String::from(key_value_equals[1]);
             signal_level = String::from(value.trim());
         }
-    }
-    // println!("{:?}", output);
 
-    println!("SSID: {ssid} | BSSID: {bssid} | IP: {ip} | Frequency: {frequency} | Level: {signal_level}");
-}
-
-#[cfg(target_os = "windows")]
- fn main() {
-    let mut ssid = String::from("");
-    let mut ip = String::from("");
-    let mut bssid = String::from("");
-    let mut frequency = String::from("");
-    let mut signal_level = String::from("");
-
-    // Getting IP
-    let output = Command::new("ipconfig")
-        .output()
-        .expect("failed to execute process");
-        
-    let output = String::from_utf8(output.stdout).expect("invalid utf8");
-    let output: Vec<&str> = output.trim().split("\n").collect();
-
-    for word in output {
-        let key = word.trim();
-
-        if key.contains("IPv4 Address") {
-            let value: Vec<&str> = word.split(":").collect();
-            let value: Vec<&str> = value[1].split("\r").collect();
-
-            ip = String::from(value[0].trim());
+        // Getting Speed (Bit Rate - MB/s)
+        if key_value_equals[0].trim() == "Rate" {
+            let mut value = String::from(key_value_equals[1]);
+            speed = String::from(value.trim());
         }
+
     }
-    // ip = String::from(output[1]);
-
-    // let output = Command::new("iwconfig")
-    //     .output()
-    //     .expect("failed to execute process");
-
-    // let output = String::from_utf8(output.stdout).expect("invalid utf8");
-    // let output: Vec<&str> = output.trim().split(" ").collect();
-
-    // for (i, word) in output.iter().enumerate() {
-    //     let key_value_colon: Vec<&str> = word.trim().split(":").collect();
-    //     let key_value_equals: Vec<&str> = word.trim().split("=").collect();
-
-    //     // Getting SSID
-    //     if key_value_colon[0].trim() == "ESSID" {
-    //         let mut value = String::from(key_value_colon[1]);
-    //         value.remove(0);
-    //         let value: Vec<&str> = value.split("\"").collect();
-    //         ssid = String::from(value[0].trim());
-    //     }
-
-    //     // Getting BSSID
-    //     if key_value_colon[0].trim() == "Point" {
-    //         let mut value = String::from(output[i+1]);
-    //         bssid = String::from(value.trim());
-    //     }
-
-    //     // Getting Frequency (GHz)
-    //     if key_value_colon[0].trim() == "Frequency" {
-    //         let mut value = String::from(key_value_colon[1]);
-    //         frequency = String::from(value.trim());
-    //     }
-
-    //     // Getting Level (dBm)
-    //     if key_value_equals[0].trim() == "level" {
-    //         let mut value = String::from(key_value_equals[1]);
-    //         signal_level = String::from(value.trim());
-    //     }
-    // }
     // println!("{:?}", output);
 
-    println!("SSID: {ssid} | BSSID: {bssid} | IP: {ip} | Frequency: {frequency} | Level: {signal_level}");
+    
+    println!("SSID: {ssid} | BSSID: {bssid} | IP: {ip} | Frequency: {frequency} | Level: {signal_level} | Speed: {speed}");
 }
